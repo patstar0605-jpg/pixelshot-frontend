4b1c22831fead177ce62630c3174e5397285f8f0
Prince Schulz <p100@MacBookAir.Home>
2026-04-27 13:58:52 +0100

    Use Railway API base URL

--- js/config.js
+++ js/config.js
@@ -1,9 +1,12 @@
 const API_BASE = 'https://pixelshot-api-production.up.railway.app';
 
 async function apiFetch(path, options = {}) {
+  const isFormData = options.body instanceof FormData;
   const res = await fetch(API_BASE + path, {
-    headers: { 'Content-Type': 'application/json', ...(options.headers || {}) },
     ...options,
+    headers: isFormData
+      ? (options.headers || {})
+      : { 'Content-Type': 'application/json', ...(options.headers || {}) },
   });
   if (!res.ok) {
     const err = await res.json().catch(() => ({}));
